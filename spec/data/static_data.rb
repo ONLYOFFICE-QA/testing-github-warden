49a234dfa7198f775f57aa19b2c1f9ea421e544b
Dmitriy Rotatyy <flamine@list.ru>
2018-12-07 16:19:50 +0300

    Add travis file

--- spec/data/static_data.rb
+++ spec/data/static_data.rb
@@ -2,7 +2,7 @@ require_relative '../../spec/data/abstract_request'
 class StaticData
   extend AbstractRequest
   ADDRESS = '0.0.0.0'.freeze
-  PORT = 9292
+  PORT = 80
   MAINPAGE = "http://#{ADDRESS}:#{PORT}".freeze
   SECRET_TOKEN = '12345'.freeze
   WRONG_HTTP_X_HUB_SIGNATURE = 'sha1=wrong_key'.freeze
