037320ce6bd2996a5f78737978886dbfc71010bf
Dmitriy Rotatyy <flamine@list.ru>
2018-12-10 18:22:50 +0300

    Add new method for every action

--- helpers/hook_direction.rb
+++ helpers/hook_direction.rb
@@ -1,6 +1,7 @@
 module HookDirection
   def find_action(object)
     result = []
+    @bugzilla = OnlyofficeBugzillaHelper::BugzillaHelper.new
     YAML.load_file('config/warden_config.yml').each do |current_pattern|
       repo_status = repository_check(object.repository.name, current_pattern)
       next unless repo_status
@@ -28,37 +29,63 @@ module HookDirection
   end
 
   def run_action(commit, action, branch)
-    bugzilla = OnlyofficeBugzillaHelper::BugzillaHelper.new
+    full_comment = create_full_comment(commit, branch)
     case action
     when 'test_action'
-      { action: 'test_action', commit: commit.message }
-    when 'close test bug'
-      bugzilla.update_bug(39_463,
-                          status: 'RESOLVED',
-                          resolution: 'FIXED')
-      { action: 'close test bug', commit: commit.message }
-    when 'close test bug and comment'
-      bugzilla.update_bug(39_463,
-                          status: 'RESOLVED',
-                          resolution: 'FIXED')
-      branch = "Commit pushed to #{branch}"
-      message = "Message: #{commit.message}"
-      author = "Author: #{commit.author.name}"
-      full_comment = "#{branch}\n#{commit.url}\n#{message}\n#{author}"
-      bugzilla.add_comment(39_463, full_comment)
-      { action: 'close test bug and comment', commit: commit.message, comment: full_comment }
-    when 'add resolved/fixed to bug and comment'
-      bugzilla.update_bug(commit.message.scan(/\d+/)[0],
-                          status: 'RESOLVED',
-                          resolution: 'FIXED')
-      branch = "Commit pushed to #{branch}"
-      message = "Message: #{commit.message}"
-      author = "Author: #{commit.author.name}"
-      full_comment = "#{branch}\n#{commit.url}\n#{message}\n#{author}"
-      bugzilla.add_comment(commit.message.scan(/\d+/)[0], full_comment)
-      { action: 'add resolved/fixed to bug and comment', commit: commit.message, comment: full_comment }
+      test_action(commit)
+    when 'close_test_bug'
+      close_test_bug(commit)
+    when 'close_test_bug_and_comment'
+      close_test_bug_and_comment(commit, full_comment)
+    when 'add_resolved_fixed_to_bug_and_comment'
+      add_resolved_fixed_to_bug_and_comment(commit, full_comment)
+    when 'only_comment_if_not_new_or_reopen'
+      only_comment_if_not_new_or_reopen(commit, full_comment)
     else
       { action: 'nothing' }
     end
+  end
+
+  def resolved_fixed_bug(bug_id)
+    OnlyofficeBugzillaHelper::BugzillaHelper.new.update_bug(bug_id,
+                                                            status: 'RESOLVED',
+                                                            resolution: 'FIXED')
+  end
+
+  def create_full_comment(commit, branch)
+    branch = "Commit pushed to #{branch}"
+    message = "Message: #{commit.message}"
+    author = "Author: #{commit.author.name}"
+    "#{branch}\n#{commit.url}\n#{message}\n#{author}"
+  end
+
+  def test_action(commit)
+    { action: 'test_action', commit: commit.message }
+  end
+
+  def close_test_bug(commit)
+    resolved_fixed_bug(39_463)
+    { action: 'close test bug', commit: commit.message }
+  end
+
+  def close_test_bug_and_comment(commit, full_comment)
+    resolved_fixed_bug(39_463)
+    @bugzilla.add_comment(39_463, full_comment)
+    { action: 'close test bug and comment', commit: commit.message, comment: full_comment }
+  end
+
+  def add_resolved_fixed_to_bug_and_comment(commit, full_comment)
+    bug_id = commit.message.scan(/\d+/)[0]
+    resolved_fixed_bug(bug_id)
+    @bugzilla.add_comment(bug_id, full_comment)
+    { action: 'add_resolved_fixed_to_bug_and_comment', commit: commit.message, comment: full_comment }
+  end
+
+  def only_comment_if_not_new_or_reopen(commit, full_comment)
+    bug_id = commit.message.scan(/\d+/)[0]
+    bug_status = @bugzilla.bug_data(bug_id)['status']
+    resolved_fixed_bug(bug_id) if bug_status != 'NEW' || bug_status != 'REOPEN'
+    @bugzilla.add_comment(bug_id, full_comment)
+    { action: 'only comment if not new or reopen', commit: commit.message, comment: full_comment }
   end
 end
