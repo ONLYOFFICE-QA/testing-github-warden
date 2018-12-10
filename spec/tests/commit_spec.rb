cfc812f1eca441cd36602046d3c0f6a4a7fefefe
Dmitriy Rotatyy <flamine@list.ru>
2018-12-10 17:19:26 +0300

    Add new test for adding comment to bug

--- spec/tests/commit_spec.rb
+++ spec/tests/commit_spec.rb
@@ -52,4 +52,28 @@ describe 'Commit smoke' do
       expect(result['resolution']).to eq('FIXED')
     end
   end
+
+  describe 'Comment' do
+    it 'comment check | add comment after closing bug' do
+      bugzilla = OnlyofficeBugzillaHelper::BugzillaHelper.new
+      bugzilla.update_bug(StaticData::BUG_ID_TEST, status: 'NEW')
+      commit_req = StaticData.commit
+      commit_req['repository']['name'] = 'test_close_and_comment'
+      commit_req['ref'] = 'refs/heads/develop'
+      commit_req['commits'][0]['message'] = 'Fix bug 39463'
+      commit_req['html_url'] = "https://githubb-fake-rebo/#{Faker::Dota.hero}"
+      commit_req['commits'][0]['author']['name'] = Faker::StarWars.character
+      responce = http.post_request(params: commit_req)
+      result_bug = bugzilla.bug_data(StaticData::BUG_ID_TEST)
+      result_comments = bugzilla.comments(StaticData::BUG_ID_TEST)
+      branch = "Commit pushed to #{commit_req['ref']}"
+      commit = commit_req['commits'][0]['url']
+      message = "Message: #{commit_req['commits'][0]['message']}"
+      author = "Author: #{commit_req['commits'][0]['author']['name']}"
+      expect(result_comments.last['text']).to eq("#{branch}\n#{commit}\n#{message}\n#{author}")
+      expect(responce.body[0]['commit']).to eq('Fix bug 39463')
+      expect(result_bug['status']).to eq('RESOLVED')
+      expect(result_bug['resolution']).to eq('FIXED')
+    end
+  end
 end
