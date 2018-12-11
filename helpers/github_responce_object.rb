a9525a1da127790462aaaefb47774090ae615488
Dmitriy Rotatyy <flamine@list.ru>
2018-12-11 14:22:15 +0300

    Fix: wrong after ping request

--- helpers/github_responce_object.rb
+++ helpers/github_responce_object.rb
@@ -4,8 +4,8 @@ class GithubResponceObjects
   attr_accessor :repository, :branch, :commits, :compare
   def initialize(params)
     @repository = Repository.new(params['repository'])
-    @branch = params['ref']
-    @commits = params['commits'].map { |commit| Commit.new(commit) }
-    @compare = params['compare']
+    @branch = params['ref'] if params['ref']
+    @commits = params['commits'].map { |commit| Commit.new(commit) } if params['commits']
+    @compare = params['compare'] if params['compare']
   end
 end
