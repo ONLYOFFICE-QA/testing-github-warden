ed44a6e5bf091d057dbc3a3aa0d0fb979100e215
Dmitriy Rotatyy <flamine@list.ru>
2018-12-19 14:19:56 +0300

    Change regular expression for add_resolved_fixed_and_comment action and add test commit message

--- spec/data/static_data.rb
+++ spec/data/static_data.rb
@@ -10,7 +10,8 @@ class StaticData
 
   CHANGE_STATUS_AND_COMMENT = ["[se] Fix bug #{BUG_ID_TEST}",
                                "[charts] Fix bug #{BUG_ID_TEST}",
-                               "Fix bug ##{BUG_ID_TEST}"].freeze
+                               "Fix bug ##{BUG_ID_TEST}",
+                               "Fixed bug ##{BUG_ID_TEST}"].freeze
   COMMENT_ONLY = ["[se] Bug #{BUG_ID_TEST}",
                   "[se] R1C1 for bug #{BUG_ID_TEST}",
                   "[se] For bug #{BUG_ID_TEST} (sort first row)"].freeze
