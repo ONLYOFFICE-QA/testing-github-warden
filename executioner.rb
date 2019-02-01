e0f423b3760e438b28e13cc631493e38ff4757c8
Dmitriy Rotatyy <flamine@list.ru>
2019-02-01 16:46:23 +0300

    Fix wrong hash structure.

--- executioner.rb
+++ executioner.rb
@@ -14,15 +14,22 @@ loop do
   data = @redis.lpop("github_warden_action")
   data = JSON.parse(data) if data
   if data.is_a?(Hash)
-    data.values do |action_data|
+    data.values[0].each do |action_data|
       case action_data['action']
       when 'add_resolved_fixed'
         add_resolved_fixed(action_data)
       when 'add_comment'
         add_comment(action_data)
+      else
+        @logger.warn "Attention! Unknown action #{action_data}"
       end
     end
+  else
+    queue = @redis.lrange('github_warden_action', 0, -1)
+    @logger.info "Queue: #{queue}"
+    if queue.empty?
+      @logger.info 'Sleep 60'
+      sleep 60
+    end
   end
-  sleep 60
-  @logger.info @redis.lrange('github_warden_action', 0, -1)
 end
