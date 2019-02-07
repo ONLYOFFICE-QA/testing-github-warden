df8c3ab59bc7c3a2d5002c6e2604e2fbd801d6be
Dmitriy Rotatyy <flamine@list.ru>
2019-02-07 16:41:03 +0300

    Add other one begin/resque: push bash element after fail

--- executioner.rb
+++ executioner.rb
@@ -10,30 +10,34 @@ diagnostic
 
 loop do
   data = @redis.lpop("github_warden_action")
-  data = JSON.parse(data) if data
-  if data.is_a?(Hash) && !data.empty?
-    @logger.info "Current data #{data}"
-    data.each do |hash, action_data|
-      next if bug_is_commented?(hash, action_data)
-      action_data.each do |action|
-        case action['action']
-        when 'add_resolved_fixed'
-          add_resolved_fixed(action)
-        when 'add_comment'
-          add_comment(action)
-        when 'nothing'
-          @logger.info "Do nothing"
-        else
-          @logger.warn "Attention! Unknown action #{action}"
+  begin
+    data = JSON.parse(data) if data
+    if data.is_a?(Hash) && !data.empty?
+      @logger.info "Current data #{data}"
+      data.each do |hash, action_data|
+        next if bug_is_commented?(hash, action_data)
+        action_data.each do |action|
+          case action['action']
+          when 'add_resolved_fixed'
+            add_resolved_fixed(action)
+          when 'add_comment'
+            add_comment(action)
+          when 'nothing'
+            @logger.info "Do nothing"
+          else
+            @logger.warn "Attention! Unknown action #{action}"
+          end
         end
       end
+    else
+      queue = @redis.lrange('github_warden_action', 0, -1)
+      @logger.info "Queue: #{queue}"
+      if queue.empty?
+        @logger.info 'Sleep 60'
+        sleep 60
+      end
     end
-  else
-    queue = @redis.lrange('github_warden_action', 0, -1)
-    @logger.info "Queue: #{queue}"
-    if queue.empty?
-      @logger.info 'Sleep 60'
-      sleep 60
-    end
+  rescue
+    @redis.lpush "github_warden_action", data.to_json
   end
 end
