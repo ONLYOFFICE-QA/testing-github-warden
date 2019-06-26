6524a813af51f28d3beb2f8f47943653c1661f6c
Pavel Lobashov <shockwavenn@gmail.com>
2019-06-26 18:29:02 +0300

    Actualize faker gem
    It deprecate several calls since v1.9.2

--- spec/tests/commit_messages_spec.rb
+++ spec/tests/commit_messages_spec.rb
@@ -13,7 +13,7 @@ describe 'Commit smoke' do
         commit_req['commits'][0]['message'] = commit_message
         commit_req['ref'] = 'refs/heads/test_branch_first'
         commit_req['html_url'] = "https://githubb-fake-rebo/test"
-        commit_req['commits'][0]['author']['name'] = Faker::StarWars.character
+        commit_req['commits'][0]['author']['name'] = Faker::Movies::StarWars.character
         responce = http.post_request(params: commit_req)
         responce_commit = responce.body[commit_req['commits'][0]['id']]
         result = responce_commit.find do |element|
@@ -33,7 +33,7 @@ describe 'Commit smoke' do
         commit_req['commits'][0]['message'] = commit_message
         commit_req['ref'] = 'refs/heads/test_branch_first'
         commit_req['html_url'] = "https://githubb-fake-rebo/test"
-        commit_req['commits'][0]['author']['name'] = Faker::StarWars.character
+        commit_req['commits'][0]['author']['name'] = Faker::Movies::StarWars.character
         responce = http.post_request(params: commit_req)
         responce_commit = responce.body[commit_req['commits'][0]['id']]
         result = responce_commit.find do |element|
@@ -52,7 +52,7 @@ describe 'Commit smoke' do
         commit_req = StaticData.commit
         commit_req['commits'][0]['message'] = commit_message
         commit_req['html_url'] = "https://githubb-fake-rebo/#{Faker::Dota.hero}"
-        commit_req['commits'][0]['author']['name'] = Faker::StarWars.character
+        commit_req['commits'][0]['author']['name'] = Faker::Movies::StarWars.character
         responce = http.post_request(params: commit_req)
         expect(responce.body).to be_empty
         expect(responce.code).to eq('200')
