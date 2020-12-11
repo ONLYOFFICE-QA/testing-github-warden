9477706ace2baead7ea1da194823e09ab1ff1b07
Pavel Lobashov <shockwavenn@gmail.com>
2020-12-11 14:36:44 +0300

    Fix rubocop `Style/StringConcatenation` cop

--- spec/lib/wrappers/http.rb
+++ spec/lib/wrappers/http.rb
@@ -28,6 +28,6 @@ class Http
   end
 
   def generate_signature(params)
-    'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['SECRET_TOKEN'], params.to_json)
+    "sha1=#{OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['SECRET_TOKEN'], params.to_json)}"
   end
 end
