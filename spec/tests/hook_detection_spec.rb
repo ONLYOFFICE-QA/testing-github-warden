ba675fc2fdf8f6acce1bc1ebe9ca3b53ebf253ba
Pavel Lobashov <shockwavenn@gmail.com>
2022-06-13 18:33:18 +0300

    Add commit

--- spec/tests/hook_detection_spec.rb
+++ spec/tests/hook_detection_spec.rb
@@ -3,15 +3,19 @@
 require 'spec_helper'
 
 describe HookDetection do
-  let(:detection) { described_class.new(instance_double(GithubResponceObjects,
-                                                        commits: [])) }
+  let(:commit) do
+    instance_double(Commit,
+                    message: 'message',
+                    url: 'url',
+                    author: instance_double(Author, name: 'AuthorName'))
+  end
+  let(:detection) do
+    described_class.new(instance_double(GithubResponceObjects,
+                                        commits: [commit]))
+  end
 
   describe '#create_full_comment' do
     it 'return a string' do
-      commit = instance_double(Commit,
-                               message: 'message',
-                               url: 'url',
-                               author: instance_double(Author, name: 'AuthorName'))
       expect(detection.create_full_comment(commit, 'branch')).to be_a(String)
     end
   end
