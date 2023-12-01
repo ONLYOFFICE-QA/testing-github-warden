0b4bdc4466d5a34367920be0d56357d4532e2375
ONLYOFFICE Testing Robot <teamlab.ruby@gmail.com>
2023-12-01 15:17:51 +0200

    Fix names

--- spec/fixtures/fixtures.rb
+++ spec/fixtures/fixtures.rb
@@ -4,17 +4,17 @@ require 'json'
 
 # Class for storing fixtures
 class Fixtures
-  def commit
+  def self.commit
     file = File.read(File.join(File.dirname(__FILE__), './fixtures_responce_objects/commit.json'))
     JSON.parse(file)
   end
 
-  def ping
+  def self.ping
     file = File.read(File.join(File.dirname(__FILE__), './fixtures_responce_objects/ping.json'))
     JSON.parse(file)
   end
 
-  def repo_match_commit
+  def self.repo_match_commit
     file = File.read(File.join(File.dirname(__FILE__), './fixtures_responce_objects/repo_match_commit.json'))
     JSON.parse(file)
   end
