5cb0037d074e8e0f9ae91c05c5bd890e320b23e5
Vitaliy Serov <vitaliy.serov@onlyoffice.com>
2024-03-22 16:59:45 +0200

    Fix more

--- scripts/check_config_duplicates.rb
+++ scripts/check_config_duplicates.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'yaml'
 
 data = YAML.load_file('config/allowed_branches.yml')
@@ -9,7 +11,7 @@ end
 duplicates = all_repository_names.select { |name| all_repository_names.count(name) > 1 }.uniq
 
 if duplicates.empty?
-  puts "No duplicates found in repository_name_array across all entries."
+  puts 'No duplicates found in repository_name_array across all entries.'
   exit 0
 else
   puts "Duplicates found in repository_name_array across all entries: #{duplicates.join(', ')}"
