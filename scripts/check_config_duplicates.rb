9b6122ec2d1fc9932adc5e748082dc740a3d9874
Vitaliy Serov <154601125+VitaliySerov@users.noreply.github.com>
2024-03-29 09:18:53 +0300

    Add CI script that check that repo config do not have duplicates (#597)
    * Add CI script that check that repo config do not have duplicates
    
    * Fix typo
    
    * Fix more
    
    * Fix duplicate
    
    * Fix task name

---
+++ scripts/check_config_duplicates.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'yaml'
+
+data = YAML.load_file('config/allowed_branches.yml')
+all_repository_names = []
+data.each do |entry|
+  all_repository_names += entry[:repository_name_array]
+end
+
+duplicates = all_repository_names.select { |name| all_repository_names.count(name) > 1 }.uniq
+
+if duplicates.empty?
+  puts 'No duplicates found in repository_name_array across all entries.'
+  exit 0
+else
+  puts "Duplicates found in repository_name_array across all entries: #{duplicates.join(', ')}"
+  exit 1
+end
