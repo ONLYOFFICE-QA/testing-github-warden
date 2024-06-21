16a920d513a6289812b255425a90c291b29aef69
Vitaliy Serov <154601125+VitaliySerov@users.noreply.github.com>
2024-06-21 14:49:48 +0300

    Add `VERSION` file with version info (#660)
    * Add `VERSION` file with version info
    
    * Fix small issues
    
    * Fix rubocop

---
+++ helpers/version_helper.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# Module for application versioning
+module VersionHelper
+  # Load version from file
+  # @return [String] version
+  def self.load_version
+    file_path = 'VERSION'
+    if File.exist?(file_path)
+      File.read(file_path).strip
+    else
+      '0.0.0'
+    end
+  end
+end
