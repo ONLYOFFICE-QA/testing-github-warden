4056e0c14012468fcc17c66adaaee0eb73447c3c
Alina Safonova <absolutecontrol@yandex.ru>
2026-03-23 15:24:42 +0300

    Fix CI

--- spec/lib/wrappers/http.rb
+++ spec/lib/wrappers/http.rb
@@ -28,7 +28,6 @@ class Http
     end
     request.body = params.to_json if params
     responce = @http.request(request)
-    puts "DEBUG RESPONSE: #{responce.code} #{responce.body}"
     responce.body = JSON.parse(responce.body)
     responce
   end
